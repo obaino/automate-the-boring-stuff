4c8a25698cc863a9a98e907d94814259c4378977
obaino <nikos.kyriazis@gmail.com>
2023-08-30 23:11:17 +0300

    updated  PQ3_Collatz

--- PQ_3_Collatz.py
+++ PQ_3_Collatz.py
@@ -1,14 +1,19 @@
+# I have added a counter to track the number of steps needed to reach 1
+
 def main():
-    number = int(input("Enter a number: "))
+    number = int(input("Enter a number:\n"))
+    step = 0
+    while collatz(number) != 1:
+        step += 1
+        print(f"step: {step} - number: {collatz(number)}")
+        number = collatz(number)
     print(collatz(number))
 
 
 def collatz(num):
     if num % 2 == 0:
-        print(num // 2)
         return num // 2
     else:
-        print(3 * num + 1)
         return 3 * num + 1
 
 
