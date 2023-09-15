b25ee8e3cd4cdcd4ae19062069cad4cb3544a25d
obaino <nikos.kyriazis@gmail.com>
2023-09-16 00:24:53 +0300

    new file:   PQ_3_Collatz.py 	modified:   conway.py

---
+++ PQ_3_Collatz.py
@@ -0,0 +1,27 @@
+# Added a counter to track the number of steps needed to reach 1
+
+
+def main():
+    while True:
+        try:
+            number = int(input("Enter a number:\n"))
+            step = 0
+            while collatz(number) != 1:
+                step += 1
+                print(f"step: {step} - number: {collatz(number)}")
+                number = collatz(number)
+            print(collatz(number))
+            break
+        except ValueError:
+            pass
+
+
+def collatz(num):
+    if num % 2 == 0:
+        return num // 2
+    else:
+        return 3 * num + 1
+
+
+if __name__ == "__main__":
+    main()
