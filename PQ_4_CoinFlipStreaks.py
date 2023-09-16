12c6510f6a3f2ca53fe821f2b1b2f3e09a8f19e9
obaino <nikos.kyriazis@gmail.com>
2023-09-16 20:43:43 +0300

    modified:   PQ_4_CoinFlipStreaks.py

--- PQ_4_CoinFlipStreaks.py
+++ PQ_4_CoinFlipStreaks.py
@@ -1,21 +1,40 @@
 import random
 
-numberOfStreaks = 0
 
-# for experimentNumber in range(10000):
-
-def results():
+def results(n):
     # Code that creates a list of 100 'heads' or 'tails' values.
     results = []
-    for i in range(100):
+    for i in range(n):
         coin = random.randint(0, 1)
         if coin == 1:
-            results.append("heads")
+            results.append("H")
         else:
-            results.append("tails")
+            results.append("T")
+    # print(results)
     return results
 
-    # Code that checks if there is a streak of 6 heads or tails in a row.
 
-print(results())
-# print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+def check(lst):
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    counter = 1
+    numberOfStreaks = 0
+    for i in range(1, len(lst)):
+        if lst[i - 1] == lst[i]:
+            counter += 1
+        else:
+            counter = 1
+        if counter >= 6:
+            numberOfStreaks += 1
+            # print(f"{i} turn:")
+            # print (f"{lst[i]} is: {counter} in a row")
+            # print(f"Number of Streaks is: {numberOfStreaks}")
+
+    return numberOfStreaks / 100
+
+
+def main():
+    print(f"Chance of streak: {check(results(100000000))}")
+
+
+if __name__ == "__main__":
+    main()
