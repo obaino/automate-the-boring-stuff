0a01ab5b8ac9a77a6b21fd3a68245def042bca0a
macnik <nikos.kyriazis@gmail.com>
2023-09-17 10:25:09 +0300

    renamed:    PQ_4_CoinFlipStreaks.py -> PQ_4_CoinFlipStreaks_v.1.py 	new file:   PQ_4_CoinFlipStreaks_v.2.py

--- PQ_4_CoinFlipStreaks_v.1.py
+++ PQ_4_CoinFlipStreaks_v.1.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+# my solution ver. 1
+
 import random
 from codetiming  import Timer
 
---
+++ PQ_4_CoinFlipStreaks_v.2.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# my solution ver. 2
+
+import random
+from codetiming  import Timer
+
+
+streakNo = 6
+flips = 100
+numberOfStreaks = 0
+
+def main():
+    t = Timer()
+    runs = int(input("How many times to run the experiment? "))
+    # flips_per_run = int(input("How many times to flip the coin per run? "))
+    # print(head_tails_list(flips_per_run))
+
+    t.start()
+    for experimentNumber in range(runs):
+        print(f"{check_for_streak(head_tails_list(flips))}")
+    t.stop()
+
+def head_tails_list(flips):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    flipsList = []
+    for i in range(flips):
+        flipsList.append(random.randint(0, 1))
+    return flipsList
+
+def check_for_streak(lst):
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+    global numberOfStreaks
+    n = 0 #index in lst
+    # run the check for a length of six elements until the end of the list
+    while n+streakNo < len(lst) - streakNo:
+        sumvalue=0                  # 
+        for i in range(streakNo):
+            sumvalue += lst[n+i]
+        if sumvalue == streakNo or sumvalue == 0:
+            numberOfStreaks += 1
+        n += 1
+    return numberOfStreaks
+
+
+if __name__ == "__main__":
+    main()
