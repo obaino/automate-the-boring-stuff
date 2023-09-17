fd315c91d44708538559b88eb89bf8f665fd282e
macnik <nikos.kyriazis@gmail.com>
2023-09-17 13:02:34 +0300

    modified:   PQ_3_Collatz.py

--- PQ_3_Collatz.py
+++ PQ_3_Collatz.py
@@ -1,16 +1,23 @@
+#!/usr/bin/env python3
+
 # Added a counter to track the number of steps needed to reach 1
+
+from codetiming  import Timer
 
 
 def main():
+    t = Timer()
     while True:
         try:
             number = int(input("Enter a number:\n"))
             step = 0
+            t.start()
             while collatz(number) != 1:
                 step += 1
                 print(f"step: {step} - number: {collatz(number)}")
                 number = collatz(number)
             print(collatz(number))
+            t.stop()
             break
         except ValueError:
             pass
