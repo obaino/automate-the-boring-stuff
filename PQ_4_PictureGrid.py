cc72dd20a2cbc2fef7bc06c84b749a1d309a4fcd
obaino <nikos.kyriazis@gmail.com>
2023-09-17 22:34:51 +0300

    new file:   PQ_4_PictureGrid.py

---
+++ PQ_4_PictureGrid.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+new_grid = []
+
+for row in range(len(grid)):
+    for column in range(len(grid[row])):
+        print(f"{grid[row][column]} is row {row} and column {column}")
+
