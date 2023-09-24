fa32a4cd63b35d94038db8ce7db9c3a1cad2c26e
obaino <nikos.kyriazis@gmail.com>
2023-09-24 08:49:38 +0300

    modified: PQ_4_PictureGrid.py

--- PQ_4_PictureGrid.py
+++ PQ_4_PictureGrid.py
@@ -15,8 +15,12 @@ height = len(grid)
 
 print(f"grid has width: {width} x height: {height}")
 new_grid = []
+counter = 0
+while counter <= width:
+        for x in range(height, 0, -1):
+                print(f"x is: {x - 1} , counter is: {counter}")
+                print(grid[x-1][counter])
+                new_grid.append(grid[x][counter])
+        counter += 1
 
-for x in range(width):
-    for y in range(height):
-
-
+print(new_grid)
