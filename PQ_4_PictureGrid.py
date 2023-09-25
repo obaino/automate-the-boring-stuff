adc7f1ec0ae5502b59b462a7c0f71454ae31bef5
macnik <nikos.kyriazis@gmail.com>
2023-09-25 14:10:44 +0300

    modified:   PQ_4_PictureGrid.py

--- PQ_4_PictureGrid.py
+++ PQ_4_PictureGrid.py
@@ -13,12 +13,26 @@ grid = [['.', '.', '.', '.', '.', '.'],
 width = len(grid[0])
 height = len(grid)
 
-print(f"grid has width: {width} x height: {height}")
-new_grid = []
-counter = 0
+print("Original grid is:")
 
-for x in range(width):
-    for y in range(height):
-        print(grid[y][x], end="")
-        
+for x in range(height):
+    print(f"This is row: {x}:_ ", end="")
+    for y in range(width):
+        print(grid[x][y], end="")
     print()
+
+print()
+print(f"grid has width: {width} x height: {height}")
+print()
+
+def rotate_list(lst):
+    # rotates lst 90 degrees
+
+    width = len(lst[0])
+    height = len(lst)
+    for x in range(width):
+        for y in range(height):
+            print(grid[y][x], end="")
+        print()
+
+rotate_list(grid)
