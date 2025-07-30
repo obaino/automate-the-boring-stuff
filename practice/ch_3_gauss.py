999d12681885518ce1cfd913d6f5e9531324289d
Nikos Kyriazis <nikos.kyriazis@gmail.com>
2025-07-30 20:21:36 +0300

    modified:   ch_3_gauss.py

--- practice/ch_3_gauss.py
+++ practice/ch_3_gauss.py
@@ -1,5 +1,7 @@
+# This program calculates the sum of numbers from 0 to upperNumber using a loop.
+upperNumber = int(input('Add up to what number? '))
 total = 0
-for num in range(101):
+for num in range(upperNumber + 1):
     total = total + num
     print('num is:', num, 'total is:', total)
 print(total)  
